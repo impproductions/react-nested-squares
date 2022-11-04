fe540f39a255852be0a67a201cda65a19625cec2
Luca Paolella <luca.paolella@gmail.com>
2022-11-04 11:25:40 +0100

    working copy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [innerSide, setInnerSide] = useState(0);
   const [windowSize, setWindowSize] = useState(0);
 
   useEffect(() => {
@@ -22,21 +21,13 @@ function App() {
 
   return (
     <div className="App" style={{ display: "flex", height: "100vh", width: "100vw", alignItems: "center", justifyContent: "center" }}>
-      {/* <div style={{transform: "rotateZ(12deg)"}}> */}
-      <NestedSquares side={Math.min(windowSize[0], windowSize[1]) / 2} nestAmount={2} nestIndex={2}>
+      <NestedSquares side={Math.min(windowSize[0], windowSize[1]) / 2} nestAmount={1}>
         <div style={{ display: "flex", height: "100%", width: "100%", border: "1px solid red" }}>
           <div style={{ flex: "1 0 100%", fontSize: "1%", display: "flex", minHeight: "100%", minWidth: "100%", border: "1px solid blue", alignItems: "center", justifyContent: "center", maxHeight: "100%", maxWidth: "100%", flexGrow: 1, fontSize: "8pt", textAlign: "justify", overflow: "hidden", whiteSpace: "normal", textOverflow: "ellipsis" }}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, aumquam nescimus blatere dispute teresio aestuntus
           </div>
         </div>
       </NestedSquares>
-      {/* </div> */}
-      {/* <div style={{ width: "300px", height: "300px", backgroundColor: "black", color: "white" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut lectus dolor. Donec maximus imperdiet nibh, ac fermentum turpis fringilla vitae. Praesent nec enim varius, ullamcorper tellus ut, mollis odio. Pellentesque et purus sem. Fusce fermentum varius tortor quis consequat. Donec vestibulum, orci ut ultrices suscipit, diam nisl imperdiet odio, sit amet tempus enim diam elementum lorem. Aenean euismod odio id ultrices maximus. Morbi posuere ultricies massa, et pretium urna condimentum eget. Vestibulum odio arcu, pellentesque eu dapibus quis, molestie nec ligula.
-      </div>
-      <div style={{ width: "300px", height: "300px", backgroundColor: "black", color: "white", transform: "scale(0.5)" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut lectus dolor. Donec maximus imperdiet nibh, ac fermentum turpis fringilla vitae. Praesent nec enim varius, ullamcorper tellus ut, mollis odio. Pellentesque et purus sem. Fusce fermentum varius tortor quis consequat. Donec vestibulum, orci ut ultrices suscipit, diam nisl imperdiet odio, sit amet tempus enim diam elementum lorem. Aenean euismod odio id ultrices maximus. Morbi posuere ultricies massa, et pretium urna condimentum eget. Vestibulum odio arcu, pellentesque eu dapibus quis, molestie nec ligula.
-      </div> */}
     </div>
   );
 }
--- src/components/Squares.js
+++ src/components/Squares.js
@@ -1,37 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 
 function NestedSquares({ side, nestAmount = 1, children, ...restProps }) {
-    return (
-        <>
-            <Squares side={side} nestAmount={nestAmount} nestIndex={nestAmount} restProps>
-                {children}
-            </Squares>
-        </>
-    );
-}
-
-function Squares({ side, nestAmount = 1, nestIndex = 0, children, ...restProps }) {
     const [angle, setAngle] = useState(0);
-    const [outerBgColor, setOuterBgColor] = useState(randomColor());
-    const [innerBgColor, setInnerBgColor] = useState(randomColor());
-    const outerBox = useRef(null);
 
-    function convertAngle(angle) {
-        return -45 + (angle % 360) / 4;
+    function innerAngle() {
+        return -angle * (nestAmount + 1) / 4;
     }
 
-    // function getParentAngle() {
-    //     return Number(outerBox.current?.parentElement.style.transform.split("rotateZ(")[1].split("deg)")[0] || 0);
-    // }
-
-    function scale(a) {
-        return calculateScale(side, convertAngle(a));
+    function scaleInner() {
+        return scaleUpTo(side, angle, nestAmount) * scale(side * scale(side, angle), angle * (nestAmount + 1));
     }
 
     useEffect(() => {
-        setOuterBgColor(randomColor());
-        setInnerBgColor(randomColor());
-
         const mouseMove = (e) => {
             const outerBox = document.getElementById("outer");
             const center = [outerBox.getBoundingClientRect().left + outerBox.getBoundingClientRect().width / 2, outerBox.getBoundingClientRect().top + outerBox.getBoundingClientRect().height / 2];
@@ -47,33 +27,41 @@ function Squares({ side, nestAmount = 1, nestIndex = 0, children, ...restProps }
         return () => window.removeEventListener("mousemove", mouseMove);
     }, []);
 
-    // function ca() {
-    //     let a = (angle + getParentAngle());
-    //     a = a > 45 ? 90 - a : a;
-    //     return -a;
-    // }
 
-    function innerAngle() {
-        return -angle * (nestAmount + 1) / 4;
-    }
+    return (
+        <>
+            <SquareInASquare side={side} angle={angle} nestAmount={nestAmount} nestIndex={nestAmount} restProps>
+                <div id="inner" style={{ border: "1px solid black", opacity: "0.2", backgroundColor: "white", minWidth: side + "px", minHeight: side + "px", maxWidth: side + "px", maxHeight: side + "px", transform: "rotateZ(" + innerAngle() + "deg) scale(" + scaleInner(side, angle) + ")", display: "flex", alignItems: "stretch", justifyContent: "stretch" }}>
+                    <div id={"content"} style={{ fontSize: "25pt", border: "1px solid green", flex: "1 0 100%" }}>
+                        {children}
+                    </div>
+                </div>
+            </SquareInASquare>
+        </>
+    );
+}
+
+function SquareInASquare({ side, angle, nestAmount = 0, nestIndex = 0, children, ...restProps }) {
+    const [outerBgColor, setOuterBgColor] = useState(randomColor());
+    const [innerBgColor, setInnerBgColor] = useState(randomColor());
+    const outerBox = useRef(null);
+
+    useEffect(() => {
+        setOuterBgColor(randomColor());
+        setInnerBgColor(randomColor());
+    }, []);
 
     return (
         <>
             <div ref={outerBox} boxtype="outer-box" id={"outer"} style={{ width: side + "px", height: side + "px", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: outerBgColor }}>
-                <div id="mid" style={{ width: side * scale(angle) + "px", height: side * scale(angle) + "px", transform: "rotateZ(" + (45 + Number(convertAngle(angle))) + "deg)", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: innerBgColor }}>
-                {/* <div id="mid" style={{ width: side * scale(angle) + "px", height: side * scale(angle) + "px", transform: "rotateZ(" + (45 + Number(convertAngle(angle))) + "deg)", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: innerBgColor }}> */}
+                <div id="mid" style={{ width: side * scale(side, angle) + "px", height: side * scale(side, angle) + "px", transform: "rotateZ(" + (45 + Number(convertAngle(angle))) + "deg)", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: innerBgColor }}>
                     {nestIndex > 0 ?
-                        <Squares side={side * scale(angle)} nestAmount={nestAmount} nestIndex={nestIndex - 1}>
+                        <SquareInASquare side={side * scale(side, angle)} angle={angle} nestAmount={nestAmount} nestIndex={nestIndex - 1}>
                             {children}
-                        </Squares>
+                        </SquareInASquare>
                         :
-                        <div id="inner" style={{ border: "1px solid black", opacity: "0.2", backgroundColor: "white", minWidth: side + "px", minHeight: side + "px", maxWidth: side + "px", maxHeight: side + "px", transform: "rotateZ(" + innerAngle() + "deg) scale(" + scale(angle) * scale(angle * (nestAmount + 1)) + ")", display: "flex", alignItems: "stretch", justifyContent: "stretch" }}>
-                        {/* <div id="inner" style={{ border: "1px solid black", backgroundColor: "white", width: (side * scale(angle)) * scale(angle * (nestAmount + 1)) + "px", height: ((side * scale(angle)) * scale(angle * (nestAmount + 1))) + "px", transform: "rotateZ(" + innerAngle() + "deg)", display: "flex", alignItems: "center", justifyContent: "center" }}> */}
-                            <div id={"content"} style={{ border: "1px solid green", flex: "1 0 100%" }}>
-                                {children}
-                            </div>
-                        </div>
-                    }
+                        children
+                    } <br />
                 </div>
             </div>
         </>
@@ -83,15 +71,24 @@ function Squares({ side, nestAmount = 1, nestIndex = 0, children, ...restProps }
 
 export default NestedSquares;
 
+function scaleUpTo(side, angle, nestAmount) {
+    return scale(side * scale(side, angle), angle) ** (nestAmount + 1);
+}
+
+function scale(side, angle) {
+    return calculateScale(side, convertAngle(angle));
+}
+
+function convertAngle(angle) {
+    return -45 + (angle % 360) / 4;
+}
 
 function calculateScale(side, angle) {
     const a = side;
     const c = a * Math.tan(angle * Math.PI / 180);
     const b = Math.sqrt((a ** 2) + (c ** 2));
 
-
     return (b / (side * Math.SQRT2));
-    // return (b / (side * Math.SQRT2)) * side;
 }
 
 function randomColor() {
